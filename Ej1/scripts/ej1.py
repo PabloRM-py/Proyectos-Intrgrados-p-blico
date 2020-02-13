b454edd010c9742bfb26f95774432eec3196e464
Roberto <pabloriveiramurcia@gmail.com>
2020-02-13 13:06:12 +0100

    ej2

--- Ej1/scripts/ej1.py
+++ Ej1/scripts/ej1.py
@@ -4,19 +4,19 @@ import rospy
 from std_msgs.msg import Int16  
 def ej1():
      pub=rospy.Publisher('numerador',Int16,queue_size=10)
-     rospy.init_node('numerados',anonymous=True)
+     rospy.init_node('numerador',anonymous=True)
      rate=rospy.Rate(10)
      i=0
      while not rospy.is_shutdown():
          try:
-             while i !=5:
+             #while i !=5:
                 num_int=int(input('Dasme ints: '))
                 #print(num_int)
                 rospy.loginfo(num_int)
                 pub.publish(num_int)
-                i=i+1
-             if i==5:
-                pass
+                #i=i+1
+             #if i==5:
+                #pass
          except IOError:
             exit('Error')
 if __name__ == '__main__':
